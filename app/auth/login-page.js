fe402a46655234ff5770bca73bf565cef60d471b
Ngoni-Sendama <ngonidzashesendama@gmail.com>
2025-11-02 15:01:26 +0530

    Login and Register page

--- app/auth/login-page.js
+++ app/auth/login-page.js
@@ -1,33 +1,18 @@
 
-// const { Frame } = require("@nativescript/core");
-
-// exports.HomePage = function () {
-//     Frame.topmost().navigate({
-//         moduleName: "home-page",
-//         clearHistory: true
-//     });
-// };
-
-
-// exports.RegisterPage = function () {
-//     Frame.topmost().navigate("auth/register-page");
-//     Frame.topmost().navigate({
-//         moduleName: "auth/register-page",
-//         clearHistory: true
-//     });
-// };
-const { Application } = require("@nativescript/core");
+const { Frame } = require("@nativescript/core");
 
 exports.HomePage = function () {
-    console.log("Navigating to Home Page...");
-    Application.resetRootView({
-        moduleName: "home-page"
+    Frame.topmost().navigate({
+        moduleName: "home-page",
+        clearHistory: true
     });
 };
 
+
 exports.RegisterPage = function () {
-    console.log("Navigating to Register Page...");
-    Application.resetRootView({
-        moduleName: "auth/register-page"
+    Frame.topmost().navigate("auth/register-page");
+    Frame.topmost().navigate({
+        moduleName: "auth/register-page",
+        clearHistory: true
     });
 };
--- app/main-page.js
+++ app/main-page.js
@@ -3,8 +3,12 @@ const { Frame } = require('@nativescript/core');
 exports.onNavigateToOnboard2 = function () {
   Frame.topmost().navigate({
     moduleName: "onboard/onboarding2-page",
-    clearHistory: true
+    clearHistory: true,      // Removes the back button
+    animated: true,          // Enables transition animation
+    transition: {
+      name: "slide",         // Options: "fade", "slide", "flip", "curl", etc.
+      duration: 300,         // Smooth speed in ms
+      curve: "easeInOut"     // Smooth easing
+    }
   });
 };
-
-
